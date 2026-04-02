6246dba86f2b97e457708dbcd56c170176765458
misut <pkr5207@gmail.com>
2026-04-03 06:39:08 +0900

    feat: add exon info command and improve exon init template

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 import std;
+import toml;
+import manifest;
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
@@ -17,7 +19,44 @@ int main(int argc, char* argv[]) {
         file << "[package]\n";
         file << "name = \"\"\n";
         file << "version = \"0.1.0\"\n";
+        file << "description = \"\"\n";
+        file << "authors = []\n";
+        file << "license = \"\"\n";
+        file << "standard = 23\n";
+        file << "\n";
+        file << "[dependencies]\n";
         std::println("created exon.toml");
+    } else if (command == "info") {
+        try {
+            auto m = manifest::load("exon.toml");
+            std::println("name: {}", m.name);
+            std::println("version: {}", m.version);
+            if (!m.description.empty())
+                std::println("description: {}", m.description);
+            if (!m.authors.empty()) {
+                std::print("authors: ");
+                for (std::size_t i = 0; i < m.authors.size(); ++i) {
+                    if (i > 0) std::print(", ");
+                    std::print("{}", m.authors[i]);
+                }
+                std::println("");
+            }
+            if (!m.license.empty())
+                std::println("license: {}", m.license);
+            std::println("standard: C++{}", m.standard);
+            if (!m.dependencies.empty()) {
+                std::println("dependencies:");
+                for (auto const& [name, ver] : m.dependencies) {
+                    std::println("  {} = \"{}\"", name, ver);
+                }
+            }
+        } catch (toml::ParseError const& e) {
+            std::println(std::cerr, "error: {}", e.what());
+            return 1;
+        } catch (std::exception const& e) {
+            std::println(std::cerr, "error: {}", e.what());
+            return 1;
+        }
     } else {
         std::println(std::cerr, "unknown command: {}", command);
         return 1;
